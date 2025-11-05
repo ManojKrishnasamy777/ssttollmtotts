8239048313dd4538003b34872a88b6be81291e97
theloftscreening <theloftscreening@gmail.com>
2025-11-06 00:47:18 +0530

    Deleted main.js.map

--- backend/dist/modules/voice/stt.interface.d.ts
+++
@@ -1,5 +0,0 @@
-export interface STTService {
-    createLiveTranscription(onTranscript: (text: string) => void, onError: (error: any) => void): Promise<any>;
-    sendAudio(connection: any, audioData: Buffer): void;
-    closeConnection(connection: any): void;
-}
