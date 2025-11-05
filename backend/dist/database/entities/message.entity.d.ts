d1703c6df10f626bd047e879932534d7a4c39fd3
manojmetabus-ceo <manojmetabus@gmail.com>
2025-11-06 00:17:08 +0530

    Deleted app.module.d.ts

--- backend/dist/database/entities/message.entity.d.ts
+++
@@ -1,9 +0,0 @@
-import { Conversation } from './conversation.entity';
-export declare class Message {
-    id: string;
-    conversationId: string;
-    role: string;
-    content: string;
-    createdAt: Date;
-    conversation: Conversation;
-}
