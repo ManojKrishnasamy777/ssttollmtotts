fa205219e777c517a73c87986490c8dab848f095
ManojKrishnasamy777 <manoj@do365tech.com>
2025-11-06 22:09:07 +0530

    Deleted main.js.map

--- backend/dist/modules/voice/elevenlabs.service.d.ts
+++
@@ -1,8 +0,0 @@
-import { ConfigService } from '@nestjs/config';
-export declare class ElevenLabsService {
-    private configService;
-    private apiKey;
-    private voiceId;
-    constructor(configService: ConfigService);
-    textToSpeech(text: string): Promise<Buffer>;
-}
